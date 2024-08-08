35723d54eddb29b711e1784cc715bd155129101c
feeroza <160626229+feeroza@users.noreply.github.com>
2024-08-08 22:17:40 +0500

    Add files via upload

---
+++ Question 23/023.js
@@ -0,0 +1,28 @@
+// 23. Conditional Tests: Write a series of conditional tests. Print a statement
+// describing each test and your prediction for the results of each test. Your code
+// should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+var price = 900;
+console.log(price == 900);
+var friend = "ahmed";
+console.log(friend == "ahmed");
+var person = "50 people";
+console.log(person == "50 people");
+var frouit = "banana";
+console.log(frouit == "banana");
+var student = "eligeble";
+console.log(student == "eligeble");
+var pric = 500;
+console.log(pric == 900);
+var friend1 = "raza";
+console.log(friend1 == "ahmed");
+var person1 = "10 people";
+console.log(person1 == "50 people");
+var frouite1 = "oringe";
+console.log(frouite1 == "banana");
+var student1 = "not eligeble";
+console.log(student1 == "eligeble");
---
+++ Question 23/023.ts
@@ -0,0 +1,55 @@
+// 23. Conditional Tests: Write a series of conditional tests. Print a statement
+// describing each test and your prediction for the results of each test. Your code
+// should look something like this:
+
+// let car = 'subaru';
+
+// console.log("Is car == 'subaru'? I predict True.")
+
+// console.log(car == 'subaru')
+
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+let price = 900;
+
+console.log(price == 900);
+
+let friend = "ahmed";
+
+console.log(friend == "ahmed");
+
+let person = "50 people";
+
+console.log(person == "50 people");
+
+let frouit = "banana";
+
+console.log(frouit == "banana");
+
+let  student = "eligeble";
+
+console.log(student == "eligeble");
+
+
+let pric = 500;
+
+console.log(pric == 900);
+
+let friend1 = "raza";
+
+console.log(friend1 == "ahmed");
+
+let person1 = "10 people";
+
+console.log(person1 == "50 people");
+
+let frouite1 = "oringe";
+
+console.log(frouite1 == "banana");
+
+let  student1= "not eligeble";
+
+console.log(student1 == "eligeble");
+
